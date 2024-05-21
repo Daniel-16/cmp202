f09db5f88441d6acdac65d96969d75a3fd1c5c7c
Daniel-16 <danieloloruntoba681@gmail.com>
2024-05-21 13:29:13 +0100

    method overloading

--- Encapsulation/Person.java
+++ Encapsulation/Person.java
@@ -2,8 +2,20 @@ package Encapsulation;
 
 public class Person {
     private String name;
-    private String schoolName = "Bingham with the difference";
+    private String schoolName;
     private String password;
+    private String matricNo;
+    private int staffNo;
+
+    public Person (String name, String schoolName, String password, String matricNo, int staffNo) {
+        this.name = name;
+        this.password = password;
+        this.schoolName = schoolName;
+        this.matricNo = matricNo;
+        this.staffNo = staffNo;
+    }
+
+    
 
     public String getName() {
         return name;
